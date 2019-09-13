9a641e56969ef2f0234f6ecbb0ddbd6bf8ac9c29
Nikos Ftylitakis <nikos.ftylitakis@feron-tech.com>
2019-09-13 18:03:04 +0300

    deactivate sock.bind for now because it prevents the socket from being able to call sendTo.

--- ucoap/ucoap.py
+++ ucoap/ucoap.py
@@ -166,9 +166,9 @@ class Coap:
 
     def start(self, port=_COAP_DEFAULT_PORT):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-        self.sock.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sockaddr = socket.getaddrinfo('127.0.0.1', port)[0][-1]
-        self.sock.bind(sockaddr)
+        # self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        # sockaddr = socket.getaddrinfo('127.0.0.1', port)[0][-1]
+        # self.sock.bind(sockaddr)
 
     def stop(self):
         if self.sock is not None:
