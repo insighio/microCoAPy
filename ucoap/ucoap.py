6ac845bf374337db87d7d769a17c9ee4dc960739
Nikos Ftylitakis <nikos.ftylitakis@feron-tech.com>
2019-09-13 18:13:40 +0300

    fix call to socket.bind that was throwing exception and restored its operation

--- ucoap/ucoap.py
+++ ucoap/ucoap.py
@@ -165,10 +165,8 @@ class Coap:
         self.port = 0
 
     def start(self, port=_COAP_DEFAULT_PORT):
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-        # self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        # sockaddr = socket.getaddrinfo('127.0.0.1', port)[0][-1]
-        # self.sock.bind(sockaddr)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind(('', port))
 
     def stop(self):
         if self.sock is not None:
