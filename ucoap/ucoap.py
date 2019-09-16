59db9ce2f5bdf45885581f8adebfc0f2bc97a365
Nikos Ftylitakis <nikos.ftylitakis@feron-tech.com>
2019-09-16 18:25:53 +0300

    add implementation to listen for incoming packets.

--- ucoap/ucoap.py
+++ ucoap/ucoap.py
@@ -1,7 +1,7 @@
 import usocket as socket
-import ustruct as struct
 import uos
 from ubinascii import hexlify
+import utime as time
 
 # Macros
 _COAP_HEADER_SIZE = 4
@@ -13,7 +13,7 @@ _COAP_DEFAULT_PORT = 5683
 
 
 def CoapResponseCode(class_, detail):
-    """no idea"""
+    """ """
     return ((class_ << 5) | (detail))
 
 
@@ -102,7 +102,8 @@ class CoapOption:
     def __init__(self, number=-1, buffer=None):
         self.number = number
         byteBuf = bytearray()
-        byteBuf.extend(buffer)
+        if buffer is not None:
+            byteBuf.extend(buffer)
         self.buffer = byteBuf
 
     def __str__(self):
@@ -178,6 +179,15 @@ class Coap:
         runningDelta = 0
         packetSize = 0
 
+        if coapPacket.contentType != COAP_CONTENT_TYPE.COAP_NONE:
+            optionBuffer = bytearray(2)
+            optionBuffer[0] = (coapPacket.contentType & 0xFF00) >> 8
+            optionBuffer[1] = (coapPacket.contentType & 0x00FF)
+            coapPacket.addOption(COAP_OPTION_NUMBER.COAP_CONTENT_FORMAT, optionBuffer)
+
+        if (coapPacket.query is not None) and (len(coapPacket.query) > 0):
+            coapPacket.addOption(COAP_OPTION_NUMBER.COAP_URI_QUERY, coapPacket.query)
+
         # make coap packet base header
         buffer = bytearray()
         buffer.append(0x01 << 6)
@@ -245,7 +255,9 @@ class Coap:
         try:
             sockaddr = socket.getaddrinfo(ip, port)[0][-1]
             status = self.sock.sendto(buffer, sockaddr)
-            print('Packet sent. Status: ', status)
+            if status > 0:
+                status = coapPacket.messageid
+            print('Packet sent. MessageId: ', status)
         except Exception as e:
             status = 0
             print('Exception while sending packet...')
@@ -274,18 +286,154 @@ class Coap:
         packet.setUriHost(ip)
         packet.setUriPath(url)
 
-        if packet.contentType != COAP_CONTENT_TYPE.COAP_NONE:
-            optionBuffer = bytearray(2)
-            optionBuffer[0] = (packet.contentType & 0xFF00) >> 8
-            optionBuffer[1] = (packet.contentType & 0x00FF)
-            packet.addOption(COAP_OPTION_NUMBER.COAP_CONTENT_FORMAT, optionBuffer)
+        return self.sendPacket(ip, port, packet)
 
-        if (packet.query is not None) and (len(packet.query) > 0):
-            packet.addOption(COAP_OPTION_NUMBER.COAP_URI_QUERY, packet.query)
+    # to be tested
+    def sendResponse(self, ip, port, messageid, payload, code, contentType, token):
+        packet = CoapPacket()
 
-        print('Packet formed: ', str(packet))
+        packet.type = COAP_TYPE.COAP_ACK
+        packet.code = code
+        packet.token = token
+        packet.payload = payload
+        packet.optionnum = 0
+        packet.messageid = messageid
+        packet.contentType = contentType
 
         return self.sendPacket(ip, port, packet)
 
+    def parseOption(self, packet, runningDelta, buffer, i):
+        option = CoapOption()
+        headlen = 1
+
+        errorMessage = (False, runningDelta, i)
+
+        if (buffer is None):
+            return errorMessage
+
+        buflen = len(buffer) - i
+
+        if (buflen < headlen):
+            return errorMessage
+
+        delta = (buffer[i] & 0xF0) >> 4
+        length = buffer[i] & 0x0F
+
+        if delta == 13:
+            headlen += 1
+            if (buflen < headlen):
+                return errorMessage
+            delta = buffer[i+1] + 13
+            i += 1
+        elif delta == 14:
+            headlen += 2
+            if (buflen < headlen):
+                return errorMessage
+            delta = ((buffer[i+1] << 8) | buffer[i+2]) + 269
+            i += 2
+        elif delta == 15:
+            return errorMessage
+
+        if length == 13:
+            headlen += 1
+            if (buflen < headlen):
+                return errorMessage
+            length = buffer[i+1] + 13
+            i += 1
+        elif length == 14:
+            headlen += 2
+            if (buflen < headlen):
+                return errorMessage
+            length = ((buffer[i+1] << 8) | buffer[i+2]) + 269
+            i += 2
+        elif length == 15:
+            return errorMessage
+
+        endOfOptionIndex = (i + 1 + length)
+
+        if endOfOptionIndex > len(buffer):
+            return errorMessage
+
+        option.number = delta + runningDelta
+        option.buffer = buffer[i+1:length]
+        packet.options.append(option)
+
+        return (True, runningDelta + delta, endOfOptionIndex)
+
+    # to be tested
+    def get(self, ip, port, url):
+        return self.send(ip, port, url, COAP_TYPE.COAP_CON, COAP_METHOD.COAP_GET, None, None, COAP_CONTENT_TYPE.COAP_NONE, None)
+
+    # to be tested
+    def put(self, ip, port, url, payload=bytearray(), queryOption=None, contentType=COAP_CONTENT_TYPE.COAP_NONE):
+        return self.send(ip, port, url, COAP_TYPE.COAP_CON, COAP_METHOD.COAP_PUT, None, payload, contentType, queryOption)
+
     def post(self, ip, port, url, payload=bytearray(), queryOption=None, contentType=COAP_CONTENT_TYPE.COAP_NONE):
         return self.send(ip, port, url, COAP_TYPE.COAP_CON, COAP_METHOD.COAP_POST, None, payload, contentType, queryOption)
+
+    def loop(self, blocking=True):
+        # deactivated for now
+        # self.sock.setblocking(blocking)
+        hasReceivedPacket = False
+        if self.sock is None:
+            return hasReceivedPacket
+
+        (buffer, remoteAddress) = self.sock.recvfrom(_BUF_MAX_SIZE)
+
+        while (buffer is not None) and (len(buffer) > 0):
+            bufferLen = len(buffer)
+            if (bufferLen < _COAP_HEADER_SIZE) or (((buffer[0] & 0xC0) >> 6) != 1):
+                (tempBuffer, tempRemoteAddress) = self.sock.recvfrom(_BUF_MAX_SIZE - bufferLen)
+                buffer.extend(tempBuffer)
+                continue
+
+            packet = CoapPacket()
+            packet.type = (buffer[0] & 0x30) >> 4
+            packet.tokenLength = buffer[0] & 0x0F
+            packet.code = buffer[1]
+            packet.messageid = 0xFF00 & (buffer[2] << 8)
+            packet.messageid |= 0x00FF & buffer[3]
+
+            if (packet.tokenLength == 0):
+                packet.token = None
+            elif (packet.tokenLength <= 8):
+                packet.token = buffer[4:packet.tokenLength]
+            else:
+                (tempBuffer, tempRemoteAddress) = self.sock.recvfrom(_BUF_MAX_SIZE - bufferLen)
+                buffer.extend(tempBuffer)
+                continue
+
+            if(_COAP_HEADER_SIZE + packet.tokenLength < bufferLen):
+                delta = 0
+                bufferIndex = _COAP_HEADER_SIZE + packet.tokenLength
+                while (len(packet.options) < _MAX_OPTION_NUM) and\
+                      (bufferIndex < bufferLen) and\
+                      (buffer[bufferIndex] != 0xFF):
+                    (status, delta, bufferIndex) = self.parseOption(packet, delta, buffer, bufferIndex)
+                    if status is False:
+                        return hasReceivedPacket
+                packet.optionnum = len(packet.options)
+
+                if ((bufferIndex + 1) < bufferLen) and (buffer[bufferIndex] == 0xFF):
+                    packet.payload = buffer[bufferIndex+1:]  # does this works?
+                else:
+                    packet.payload = None
+
+            hasReceivedPacket = True
+
+            if packet.type == COAP_TYPE.COAP_ACK:
+                if self.resposeCallback is not None:
+                    self.resposeCallback(packet, remoteAddress)
+
+            # deactivated for now - read only one packet per call
+            # next packet
+            # (buffer, remoteAddress) = self.sock.recvfrom(_BUF_MAX_SIZE)
+            return hasReceivedPacket
+
+        return hasReceivedPacket
+
+    # deactivated for now
+    # def poll(self, timeoutMs=-1):
+    #     start_time = time.ticks_ms()
+    #     while not self.loop(False) and ((time.ticks_ms()-start_time < timeoutMs)):
+    #         time.sleep_ms(10)
