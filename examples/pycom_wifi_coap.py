8f03a01f80e9354932094a767d33303dff6b3afd
Nikos Ftylitakis <nikos.ftylitakis@feron-tech.com>
2019-09-18 12:10:01 +0300

    refactoring on the example to include a call to poll to wait for incoming respose to a request that we have already sent. removed the try/catch to make the example simpler

--- examples/pycom_wifi_coap.py
+++ examples/pycom_wifi_coap.py
@@ -10,35 +10,41 @@ _SERVER_IP = '192.168.1.2'
 _SERVER_PORT = 5683  # default CoAP port
 _COAP_URL = 'to/a/path'
 
-nets = wlan.scan()
-for net in nets:
-    if net.ssid == _MY_SSID:
-        print('Network found!')
-        wlan.connect(net.ssid, auth=(net.sec, _MY_PASS), timeout=5000)
-        while not wlan.isconnected():
-            machine.idle()  # save power while waiting
-        print('WLAN connection succeeded!')
-        break
 
-if not wlan.isconnected():
-    print("no network found")
-else:
-    client = microcoapy.Coap()
-    try:
-        print('Starting CoAP client...')
-        client.start()
-        print('OK')
-    except Exception:
-        print('Failed')
+def connectToWiFi():
+    nets = wlan.scan()
+    for net in nets:
+        if net.ssid == _MY_SSID:
+            print('Network found!')
+            wlan.connect(net.ssid, auth=(net.sec, _MY_PASS), timeout=5000)
+            while not wlan.isconnected():
+                machine.idle()  # save power while waiting
+            print('WLAN connection succeeded!')
+            break
 
-    message = "test"
+    return wlan.isconnected()
 
-    print("About to post message: ")
-    bytesTransferred = client.post(_SERVER_IP,
-                                   _SERVER_PORT,
-                                   _COAP_URL,
-                                   message,
-                                   None,
-                                   microcoapy.COAP_CONTENT_TYPE.COAP_TEXT_PLAIN)
-    print("Sent bytes: ", bytesTransferred)
-    client.stop()
+
+def receivedMessageCallback(packet, sender):
+        print('Message received:', packet, ', from: ', sender)
+
+
+connectToWiFi()
+
+client = microcoapy.Coap()
+# setup callback for incoming respose to a request
+client.resposeCallback = receivedMessageCallback
+
+# Starting CoAP...
+client.start()
+
+# About to post message...
+bytesTransferred = client.post(_SERVER_IP, _SERVER_PORT, _COAP_URL, "test",
+                               None, microcoapy.COAP_CONTENT_TYPE.COAP_TEXT_PLAIN)
+print("Sent bytes: ", bytesTransferred)
+
+# wait for respose to our request for 2 seconds
+client.poll(2000)
+
+# stop CoAP
+client.stop()
