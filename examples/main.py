f65c8ddec062fcff646a527bab3f1153acc1501d
Nikos Ftylitakis <nikos.ftylitakis@feron-tech.com>
2019-12-18 13:43:48 +0200

    updated the main.py for convenience to have the names of all the example applications already inside

--- examples/main.py
+++ examples/main.py
@@ -1,11 +1,7 @@
-# import sys
-# sys.path.append("/flash")
-
 print("hello from microCoAPy example main")
 
 import pycom_wifi_coap_client
 #import pycom_wifi_coap_server
-
 #import esp_wifi_coap_server
 #import esp_wifi_coap_client
 #import esp_wifi_coap_client_custom_socket
